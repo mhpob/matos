898a3f75e55cbe79075d60d65847c60739efdfea
Mike O'Brien <mikeob9@gmail.com>
2023-02-24 16:34:11 -0500

    initial tag test

--- tests/testthat/test-make_tag_push_summary.R
+++ tests/testthat/test-make_tag_push_summary.R
@@ -1,19 +1,44 @@
-# td <- file.path(tempdir(), 'matos_test_files')
-# dir.create(td)
-#
-# download.file('https://members.oceantrack.org/data/repository/pbsm/detection-extracts/pbsm_matched_detections_2018.zip',
-#                destfile = file.path(td, 'pbsm_matched_detections_2018.zip'))
-# unzip(file.path(td, 'pbsm_matched_detections_2018.zip'),
-#       exdir = td)
-#
-#
-# make_tag_push_summary(matched = file.path(td, 'pbsm_matched_detections_2018.csv'))
+## Simulate non-ACT project
+td <- file.path(tempdir(), 'matos_test_files')
+dir.create(td)
 
+download.file('https://members.oceantrack.org/data/repository/pbsm/detection-extracts/pbsm_matched_detections_2018.zip',
+               destfile = file.path(td, 'pbsm_matched_detections_2018.zip'))
+unzip(file.path(td, 'pbsm_matched_detections_2018.zip'),
+      exdir = td)
 
-## NO TESTS YET. THIS IS A PLACEHOLDER
+matched <- file.path(td, 'pbsm_matched_detections_2018.csv')
 
 
 
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+
+## ACT project
+test_that('ACT projects are summarized', {
+
+  expect_no_error(
+    make_tag_push_summary(
+      matos_project = 161
+    )
+  )
+
+  expect_true(any(grepl('tag_push_summary', list.files(getwd()))))
+
 })
+
+# Non-ACT project
+test_that("Non-ACT projects are summarized", {
+
+  expect_no_error(
+    make_tag_push_summary(
+      matched = matched,
+      since = '2018-05-06'
+    )
+  )
+
+  expect_true(any(grepl('tag_push_summary', list.files(getwd()))))
+
+
+})
+
+
+unlink(td, recursive = T)
