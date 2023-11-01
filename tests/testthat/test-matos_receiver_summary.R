8228d84a04eead5988d621a7a774ba22e935d1c9
Mike O'Brien <mikeob9@gmail.com>
2023-11-01 13:44:07 -0400

    Test for https://github.com/mhpob/otndo/issues/4

--- tests/testthat/test-matos_receiver_summary.R
+++ tests/testthat/test-matos_receiver_summary.R
@@ -10,3 +10,16 @@ test_that('ACT projects are summarized', {
   expect_true(any(grepl('receiver_push_summary', list.files(getwd()))))
 
 })
+
+## ACT project with receiver deployment data in unlabeled sheet
+test_that('ACT project with unlabeled deployment sheet is summarized', {
+
+  expect_no_error(
+    matos_receiver_summary(
+      matos_project = 164
+    )
+  )
+
+  expect_true(any(grepl('receiver_push_summary', list.files(getwd()))))
+
+})
