1542e1f3a9bcaae70966fe27cedf79f49431299d
Mike O'Brien <mikeob9@gmail.com>
2023-11-21 16:25:01 -0500

    move onattach to zzz

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,11 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage(
+    cli::cli_text(
+      "By continuing, you are agreeing to the ACT Network MATOS User Agreement and Data Policy, Version 1.2:\n\n",
+      "{.url https://matos.asascience.com/static/MATOS.User.Agreement.V1.1.pdf}")
+  )
+}
+
 .onLoad <- function(libname, pkgname) {
   list_my_projects <<- memoise::memoise(list_my_projects)
   list_projects <<- memoise::memoise(list_projects)
