0ee5fde69631705748c5282720180aec53cb0029
mhpob <mhpob@users.noreply.github.com>
2024-01-23 20:12:29 +0000

    Style code (GHA)

--- tests/testthat/test-get_file_list.R
+++ tests/testthat/test-get_file_list.R
@@ -1,7 +1,7 @@
 test_that("errors with incorrect project", {
   expect_error(
-    get_file_list(8675309, 'dataextractionfiles'),
-    'No file lists returned\\.'
+    get_file_list(8675309, "dataextractionfiles"),
+    "No file lists returned\\."
   )
 })
 
@@ -10,30 +10,28 @@ test_that("returns correct class", {
   skip_on_Runiverse()
 
   expect_s3_class(
-    file_list <- get_file_list(161, 'dataextractionfiles'),
-    'response'
+    file_list <- get_file_list(161, "dataextractionfiles"),
+    "response"
   )
 
   expect_s3_class(
     httr::content(file_list),
-    c('xml_document', 'xml_node'),
+    c("xml_document", "xml_node"),
     exact = TRUE
   )
-
 })
 
 
 test_that("contains a table", {
   skip_on_Runiverse()
 
-  tab_attr <- get_file_list(161, 'dataextractionfiles') |>
+  tab_attr <- get_file_list(161, "dataextractionfiles") |>
     httr::content() |>
     rvest::html_element(xpath = '//*[@id="content"]/table') |>
-    rvest::html_attr('class')
+    rvest::html_attr("class")
 
   expect_equal(
     tab_attr,
-    'tableContent'
+    "tableContent"
   )
-
 })
