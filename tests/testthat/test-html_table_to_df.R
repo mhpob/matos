5a8c86bad485550384b892c868c88ca905f8bff8
Mike O'Brien <mikeob9@gmail.com>
2024-01-23 14:31:02 -0500

    html_table_to_df test

---
+++ tests/testthat/test-html_table_to_df.R
@@ -0,0 +1,43 @@
+login_check()
+
+test_that("converts to df", {
+  # skip test on Runiverse
+  skip_on_Runiverse()
+
+  html_file_list <- get_file_list(161, 'dataextractionfiles')
+
+
+  expect_s3_class(
+    html_df <- html_table_to_df(html_file_list),
+    'data.frame'
+  )
+  expect_named(
+    html_df,
+    c('project', 'file_type', 'detection_type', 'detection_year', 'upload_date',
+      'file_name', 'url')
+  )
+})
+
+
+
+test_that("handles projects that have no files", {
+  html_file_list <- readRDS(
+    test_path('testfiles/html_file_list_nofiles.RDS')
+  )
+
+  expect_s3_class(
+    html_df <- html_table_to_df(html_file_list),
+    'data.frame'
+  )
+
+  expect_equal(
+    nrow(html_df),
+    0
+  )
+
+  expect_named(
+    html_df,
+    c('project', 'file_type', 'detection_type', 'detection_year', 'upload_date',
+      'file_name', 'url')
+  )
+})
