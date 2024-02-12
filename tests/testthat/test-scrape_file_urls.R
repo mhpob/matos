00fc67164dd2f9eec66cca61439cba584290c9f9
Mike O'Brien <mikeob9@gmail.com>
2024-02-12 10:03:57 -0500

    add scrape_file_urls test

---
+++ tests/testthat/test-scrape_file_urls.R
@@ -0,0 +1,38 @@
+test_that("returns correct class", {
+  skip_on_cran()
+
+  html_file_list <- get_file_list(161, "dataextractionfiles")
+
+  urls <- scrape_file_urls(html_file_list)
+
+  expect_type(
+    urls,
+    'character'
+  )
+})
+
+
+test_that("URLs were built correctly", {
+  skip_on_cran()
+
+  html_file_list <- get_file_list(161, "dataextractionfiles")
+
+  urls <- scrape_file_urls(html_file_list)
+
+  expect_true(
+    all(
+      grepl(
+        "https://matos.asascience.com/projectfile/downloadExtraction",
+        dirname(urls)
+      )
+    )
+  )
+  expect_true(
+    all(
+      grepl(
+        '161_',
+        basename(urls)
+      )
+    )
+  )
+})
