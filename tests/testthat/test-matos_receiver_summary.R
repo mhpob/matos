8be4b7174b2a56993bd6150af330f31a2dad57ff
Mike O'Brien <mikeob9@gmail.com>
2024-02-12 11:18:32 -0500

    add more informative tests to otndo wrappers

--- tests/testthat/test-matos_receiver_summary.R
+++ tests/testthat/test-matos_receiver_summary.R
@@ -3,13 +3,47 @@ test_that("ACT projects are summarized", {
   skip_on_cran()
   skip_on_runiverse()
 
-  expect_no_error(
+  # matos::matos_receiver_summary messages
+  expect_message(
     matos_receiver_summary(
       matos_project = 161
+    ),
+    "Finding extraction files"
+  ) |>
+    expect_message("Files found") |>
+    expect_message("Downloading qualified detections") |>
+    expect_message("Downloading files") |>
+    expect_message("File\\(s\\) saved to") |>
+    expect_message("Unzipping files") |>
+    expect_message("File\\(s\\) unzipped to") |>
+    expect_message("Done") |>
+    expect_message("Downloading unqualified detections") |>
+    expect_message("Downloading files") |>
+    expect_message("File\\(s\\) saved to") |>
+    expect_message("Unzipping files") |>
+    expect_message("File\\(s\\) unzipped to") |>
+    expect_message("Done") |>
+    expect_message("Downloading deployment detections") |>
+    expect_message("Downloading files") |>
+    expect_message("File\\(s\\) saved to") |>
+    expect_message("Unzipping files") |>
+    expect_message("File\\(s\\) unzipped to") |>
+    expect_message("Done") |>
+    expect_message("Asking OTN GeoServer for project information") |>
+    expect_message("Writing report") |>
+    # otndo messages
+    expect_output("processing file") |>
+    expect_output("output file") |>
+    expect_output("Output created") |>
+    # matos again
+    expect_message("Done")
+
+
+  expect_true(
+    any(
+      grepl("receiver_push_summary", list.files(getwd()))
     )
   )
-
-  expect_true(any(grepl("receiver_push_summary", list.files(getwd()))))
 })
 
 ## ACT project with receiver deployment data in unlabeled sheet
@@ -17,11 +51,23 @@ test_that("ACT project with unlabeled deployment sheet is summarized", {
   skip_on_cran()
   skip_on_runiverse()
 
-  expect_no_error(
+  suppressMessages(
     matos_receiver_summary(
       matos_project = 164
     )
   )
 
-  expect_true(any(grepl("receiver_push_summary", list.files(getwd()))))
+  expect_true(
+    any(
+      grepl("receiver_push_summary", list.files(getwd()))
+    )
+  )
+})
+
+
+test_that("error with no way to find files", {
+  expect_error(
+    matos_receiver_summary(),
+    "Must provide an ACT.*qualified.*unqualified.*deployment"
+  )
 })
--- tests/testthat/test-matos_tag_summary.R
+++ tests/testthat/test-matos_tag_summary.R
@@ -3,11 +3,35 @@ test_that("ACT projects are summarized", {
   skip_on_cran()
   skip_on_runiverse()
 
-  expect_no_error(
-    matos_tag_summary(
-      matos_project = 87
-    )
-  )
+
+    expect_message(
+      matos_tag_summary(matos_project = 87)
+    ) |>
+      expect_message("Finding extraction files") |>
+      expect_message("Files found") |>
+      expect_message("Downloading matched detections") |>
+      expect_message("Downloading files") |>
+      expect_message("File\\(s\\) saved to") |>
+      expect_message("Unzipping files") |>
+      expect_message("File\\(s\\) saved to")|>
+      expect_message("Asking OTN GeoServer for project information") |>
+      expect_message("Writing report") |>
+      # otndo
+      expect_output("processing file") |>
+      expect_output("output file") |>
+      expect_output("Output created") |>
+      # matos
+      expect_message("Done")
 
   expect_true(any(grepl("tag_push_summary", list.files(getwd()))))
+})
+
+
+
+
+test_that("errors with no way to find files", {
+expect_error(
+  matos_tag_summary(),
+  "Must provide an ACT.*OTN-matched data"
+)
 })
