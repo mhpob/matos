6cfa7214fb8c8b784d26e7eb3824441b9498dd09
Mike O'Brien <mikeob9@gmail.com>
2024-02-13 17:33:36 -0500

    make fake .Renviron

--- tests/testthat/test-set_matos_credentials.R
+++ tests/testthat/test-set_matos_credentials.R
@@ -1,9 +1,47 @@
 test_that("errors if credentials exist", {
-  skip_on_cran()
-  skip_on_runiverse()
+  # Create faux creds
+  home <- Sys.getenv("HOME")
+  renv_path <- file.path(home, ".Renviron")
 
+  if (!file.exists(renv_path)) {
+    file.create(renv_path)
+  }
+
+  renv <- readLines(renv_path)
+
+  write("MATOS_USER=\"abc\"", renv_path, sep = "\n", append = TRUE)
+  write("MATOS_USER=\"123\"", renv_path, sep = "\n", append = TRUE)
+
+  # Test
   expect_error(
     set_matos_credentials(),
     "Some MATOS credentials already exist"
   )
+
+  # Clean up
+  write(renv, renv_path)
+})
+
+test_that("", {
+  skip_on_os("windows")
+  # Create faux creds
+  home <- Sys.getenv("HOME")
+  renv_path <- file.path(home, ".Renviron")
+
+  if (!file.exists(renv_path)) {
+    file.create(renv_path)
+  }
+
+  renv <- readLines(renv_path)
+
+  # Test
+  expect_warning(
+    set_matos_credentials(),
+    "your platform is not supported"
+  ) |>
+    expect_warning("your platform is not supported") |>
+    expect_message("Your MATOS credentials have been stored in your \\.Renviron")
+
+  # Clean up
+  write(renv, renv_path)
 })
