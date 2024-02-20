27b008c2e08d038f3c949400bbe191c82f6a31f5
Mike O'Brien <mikeob9@gmail.com>
2024-02-20 14:32:58 -0500

    regular message that can be suppressed until cli adds the feature

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,11 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage(
-    cli::cli_text(
-      "By continuing, you are agreeing to the ACT Network MATOS User Agreement and Data Policy, Version 1.2:\n\n",
-      "{.url https://matos.asascience.com/static/MATOS.User.Agreement.V1.1.pdf}"
-    )
+    # cli::cli_alert_info(
+      c(
+        "By continuing, you are agreeing to the ACT Network MATOS User Agreement and Data Policy, Version 1.2:\n\n",
+        "<https://matos.asascience.com/static/MATOS.User.Agreement.V1.1.pdf>"
+      )
+    # )
   )
 
   if (!curl::has_internet()) {
